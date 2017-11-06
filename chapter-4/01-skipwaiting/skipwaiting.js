984379ac333125106b4861379c71f68ba4c5b998
Corsin Capol <corsin.capol@htwchur.ch>
2017-11-06 20:21:23 +0100

    skipwaiting example added

---
+++ chapter-4/01-skipwaiting/skipwaiting.js
@@ -0,0 +1,20 @@
+const expectedCaches = ['skip-waiting'];
+
+self.addEventListener('install', event => {
+  console.log('installing...');
+  
+  self.skipWaiting();
+  
+  event.waitUntil(
+    caches.open('skip-waiting').then(cache => cache.add('crazyman.svg'))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Images from https://openclipart.org
+  if (url.origin == location.origin && url.pathname.endsWith('alien.svg')) {
+    event.respondWith(caches.match('crazyman.svg'));
+  }
+});
