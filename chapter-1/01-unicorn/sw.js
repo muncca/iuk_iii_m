32ff8c0989f775cb4ef89762c03314b23cf5f73b
Corsin Capol <corsin.capol@htwchur.ch>
2017-11-06 20:52:27 +0100

    lifecycle example

--- chapter-1/01-unicorn/sw.js
+++ chapter-1/01-unicorn/sw.js
@@ -1,4 +1,13 @@
+self.addEventListener('install', event => {
+  console.log('installing...');
+});
+
+self.addEventListener('activate', event => {  
+  console.log('activate...');
+});
+
 self.addEventListener('fetch', function(event) {
+	console.log('fetch...');
 	if(/\.png$/.test(event.request.url)) {
 		event.respondWith(fetch('unicorn.png'));
 	}
