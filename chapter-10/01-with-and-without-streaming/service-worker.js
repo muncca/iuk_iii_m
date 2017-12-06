1b1523cac87ebb96959f32a77d4b06795d981c1b
Corsin Capol <corsin@muncca.com>
2017-12-06 09:38:30 +0100

    lorem ipsum text for streaming added

--- chapter-10/01-with-and-without-streaming/service-worker.js
+++ chapter-10/01-with-and-without-streaming/service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('fetch', event => {
 });
 
 function htmlStream() {
-  const html = 'html goes here...';
+  const html = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium velit non elit hendrerit placerat. Donec vel risus nec ligula tristique condimentum. Nam hendrerit, dolor id accumsan ullamcorper, lectus mauris fringilla orci, quis feugiat nibh sem at tellus. Mauris et cursus ligula. Aliquam erat volutpat. Nulla rhoncus aliquam ex ut porta. Quisque condimentum dignissim metus, sit amet efficitur odio. Vivamus vel egestas nisl, ac suscipit sem. Pellentesque posuere varius dui ut venenatis. Curabitur pulvinar lobortis justo in bibendum. Etiam maximus augue in eleifend gravida. Aliquam vestibulum luctus dui, eu lacinia justo maximus at.';
 
   const stream = new ReadableStream({
     start: controller => {
